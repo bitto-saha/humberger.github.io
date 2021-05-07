8a98f1029cee726576f23482f9e6acd6fe291928
BittoSahaTrinoy <79255327+BittoSahaTrinoy@users.noreply.github.com>
2021-05-07 22:13:03 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function bk(){
+  document.getElementById("a").style.display="initial";
+  document.getElementById("b").style.display="none";
+  document.getElementById("o").style.display="none";
+   document.getElementById("x").style.display="initial";
+   document.getElementById("c").style.display="inline-block";
+}
+
+
+function sk(){
+  
+  
+  document.getElementById("a").style.display = "none";
+  document.getElementById("b").style.display = "initial";
+  document.getElementById("o").style.display = "initial";
+  document.getElementById("x").style.display = "none";
+  document.getElementById("c").style.display="none";
+}
